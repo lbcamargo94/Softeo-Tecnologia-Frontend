5e6ee1f02281a20b2f72fa34a9543c3693d06f8a
Lucas Camargo <lb.camargo94@gmail.com>
2022-09-14 00:45:01 -0300

    feat(): Criado novo Figure Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Figure } from "react-bootstrap";
 
 export default function Logo() {
   return (
-    <div>Logo</div>
-  )
+    <div>
+      <Figure>
+        <Figure.Image
+          width={171}
+          height={180}
+          alt="171x180"
+          src="https://w7.pngwing.com/pngs/302/443/png-transparent-white-tooth-dentistry-dental-restoration-crown-orthodontics-teeth-text-heart-people.png"
+        />
+      </Figure>
+    </div>
+  );
 }
