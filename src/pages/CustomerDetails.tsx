fc9a6b63dd1e28ce865914c429e7a4a1e75d6b78
Lucas Camargo <lb.camargo94@gmail.com>
2022-09-21 18:50:15 -0300

    feat(): Estruturado elementos da página Customer Details

--- src/pages/CustomerDetails.tsx
+++ src/pages/CustomerDetails.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function CustomerDetails() {
-  return <div>CustomerDetails</div>;
+  return (
+    <div>
+      CustomerDetails
+      <div>
+        <div>Profile</div>
+        <button>Editar</button>
+        <button>Excluir</button>
+      </div>
+      <div>
+        <h1>Crontratos</h1>
+        <button>Novo Contrato</button>
+      </div>
+      <div>Lista de Contratos do Cliente</div>
+    </div>
+  );
 }
